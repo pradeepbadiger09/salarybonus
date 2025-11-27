2c08029b58d8571bde00851ba7531acb52fa70d2
pradeepbadiger09 <pradeepbadiger618@gmail.com>
2025-11-27 14:34:37 +0530

    Update salarybouns.py

--- salarybouns.py
+++ salarybouns.py
@@ -1,8 +1,18 @@
 import sys
-if len(sys. argv) != 2:
-print("Usage: python salary_bonus py ‹sala! sys.exit()
-salary = float(sys.argv[1])
+
+if len(sys.argv) == 2:
+    script_name = sys.argv[0]
+    salary = float(sys.argv[1])
+    print("User provided salary:")
+else:
+    script_name = sys.argv[0]
+    salary = 20000.0
+    print("No input given - using default salary:")
+
 bonus = salary * 0.10
-total = salary + bonus
-print("Bonus Amount:" print("Total Salary:"
-bonus) total)
+total_salary = salary + bonus
+
+print("Script Name:", script_name)
+print("Salary:", salary)
+print("Bonus Amount:", bonus)
+print("Total Salary After Bonus:", total_salary)
