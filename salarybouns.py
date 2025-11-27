3a0609da6992d229bf098f3ced7f75f7828cb8aa
pradeepbadiger09 <pradeepbadiger618@gmail.com>
2025-11-27 14:20:07 +0530

    Update salarybouns.py

--- salarybouns.py
+++ salarybouns.py
@@ -1,11 +1,8 @@
 import sys
-if len (sys.argv) != 2:
-print("Usage: python temp_alert.py ‹temperi
-sys.exit()
-temp = float(sys.argv[1])
-if temp < 15:
-print("Cold")
-elif 15 <= temp <= 30:
-print("Normal")
-else:
-print("Hot")
+if len(sys. argv) != 2:
+print("Usage: python salary_bonus py ‹sala! sys.exit()
+salary = float(sys.argv[1])
+bonus = salary * 0.10
+total = salary + bonus
+print("Bonus Amount:" print("Total Salary:"
+bonus) total)
